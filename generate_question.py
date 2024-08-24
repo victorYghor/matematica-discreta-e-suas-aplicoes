75bdc5105318a6ee6a0fcd275df9644c6652b5ec
vitorYghor <devictoryghor@gmail.com>
2024-08-24 17:04:24 -0300

    Create a python scprit for automatizating the merge of pdf files

---
+++ generate_question.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python3
+
+from pdfmerge import pdfmerge
+import sys
+
+# pegar os argumentos separa as variaveis 
+print('funcoinou')
+
+# fazer string interpol para cirar o nome dos arquivos e o nome do arquivo resultado e gerar ele.
